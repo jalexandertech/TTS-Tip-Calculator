a2bb1668a86690094705816df6b05f5c3d3f2165
dvisionst <jcflores.edu>
2022-08-08 21:37:31 -0700

    Adding the calculator functionality using Js

---
+++ tip_calc.js
@@ -0,0 +1,29 @@
+function Meal(){
+    let total = parseFloat(document.getElementById('bill').value);
+    let percentage = parseFloat(document.getElementById('quality').value);
+    let guests = parseFloat(document.getElementById('guests').value);
+    let added = total * percentage;
+    let final = total + added;
+    let split = final/guests;
+}
+
+
+class Dinner{
+constructor(final, split){
+		let total = parseFloat(document.getElementById('bill').value);
+    let percentage = parseFloat(document.getElementById('quality').value);
+    let guests = parseFloat(document.getElementById('guests').value);
+    let added = total * percentage;
+    let fin = (total + added).toFixed(2);
+    let zz = (fin/guests).toFixed(2);
+    this.final = fin;
+    this.split = zz;
+  	}
+}
+
+function displayOut(){
+	let x = new Dinner();
+  
+	let htmlString = "Each guest pays $"+ x.split +" in tips. This equates to a total of: $"+ x.final;
+  document.getElementById("results").innerHTML = htmlString;
+}
