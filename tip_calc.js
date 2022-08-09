e823572627b815ceab53553f26a9588c8a528266
Tkawamura02 <86638759+Tkawamura02@users.noreply.github.com>
2022-08-09 14:40:28 -0700

    Tiffany's updates
    style.css was updated for aesthetics: changed calculator background color, margin, and border radius. Fixed the font-sizes, font-weight, and margins of certain areas.
    tip_calc.js was updated and the htmlString was split into two to avoid shifting of calculator to left (but now calculator shifts up a little bit the first time you use it)
    tip_calc.html was updated to match the javascript edit so that htmlString1 and htmlString2 has target locations.

--- tip_calc.js
+++ tip_calc.js
@@ -1,17 +1,19 @@
 class Dinner{
-constructor(final, split){
-    let total = parseFloat(document.getElementById('bill').value);
-    let percentage = parseFloat(document.getElementById('quality').value);
-    let guests = parseFloat(document.getElementById('guests').value);
-    let full_bill = (total + (total*percentage)).toFixed(2);
-    let shares = (full_bill/guests).toFixed(2);
-    this.final = full_bill;
-    this.split = shares;
-  	}
-}
-
-function displayOut(){
-	let meal = new Dinner();
-  	let htmlString = "Each guest pays $"+ meal.split +". This equates to a total of: $"+ meal.final;
-  document.getElementById("results").innerHTML = htmlString;
-}
+    constructor(final, split){
+        let total = parseFloat(document.getElementById('bill').value);
+        let percentage = parseFloat(document.getElementById('quality').value);
+        let guests = parseFloat(document.getElementById('guests').value);
+        let full_bill = (total + (total*percentage)).toFixed(2);
+        let shares = (full_bill/guests).toFixed(2);
+        this.final = full_bill;
+        this.split = shares;
+          }
+    }
+    
+    function displayOut(){
+        let meal = new Dinner();
+          let htmlString1 = "Each guest pays $"+ meal.split + ".";
+          let htmlString2 = "This equates to a total of: $"+ meal.final + ".";
+      document.getElementById("eachguest").innerHTML = htmlString1;
+      document.getElementById("totalof").innerHTML = htmlString2;
+    }
