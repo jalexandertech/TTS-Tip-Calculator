f5703684293fa27b5515119799cd2b5660a6bf28
dvisionst <62859441+dvisionst@users.noreply.github.com>
2022-08-09 09:47:52 -0700

    Update tip_calc.js
    Cleaned up variables and variable names

--- tip_calc.js
+++ tip_calc.js
@@ -3,16 +3,15 @@ constructor(final, split){
     let total = parseFloat(document.getElementById('bill').value);
     let percentage = parseFloat(document.getElementById('quality').value);
     let guests = parseFloat(document.getElementById('guests').value);
-    let added = total * percentage;
-    let fin = (total + added).toFixed(2);
-    let zz = (fin/guests).toFixed(2);
-    this.final = fin;
-    this.split = zz;
+    let full_bill = (total + (total*percentage)).toFixed(2);
+    let shares = (full_bill/guests).toFixed(2);
+    this.final = full_bill;
+    this.split = shares;
   	}
 }
 
 function displayOut(){
-	let x = new Dinner();
-  	let htmlString = "Each guest pays $"+ x.split +". This equates to a total of: $"+ x.final;
+	let meal = new Dinner();
+  	let htmlString = "Each guest pays $"+ meal.split +". This equates to a total of: $"+ meal.final;
   document.getElementById("results").innerHTML = htmlString;
 }
