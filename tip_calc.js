3fdde51809b57f9fa7b6355dc6b22f68aa7c0b13
dvisionst <jcflores.edu>
2022-08-09 18:31:30 -0700

    Added a line to display just the tip amount

--- tip_calc.js
+++ tip_calc.js
@@ -1,19 +1,23 @@
 class Dinner{
-    constructor(final, split){
-        let total = parseFloat(document.getElementById('bill').value);
-        let percentage = parseFloat(document.getElementById('quality').value);
-        let guests = parseFloat(document.getElementById('guests').value);
-        let full_bill = (total + (total*percentage)).toFixed(2);
-        let shares = (full_bill/guests).toFixed(2);
-        this.final = full_bill;
-        this.split = shares;
-          }
-    }
-    
-    function displayOut(){
-        let meal = new Dinner();
-          let htmlString1 = "Each guest pays $"+ meal.split + ".";
-          let htmlString2 = "This equates to a total of: $"+ meal.final + ".";
-      document.getElementById("eachguest").innerHTML = htmlString1;
-      document.getElementById("totalof").innerHTML = htmlString2;
-    }
+  constructor(final, split){
+      let total = parseFloat(document.getElementById('bill').value);
+      let percentage = parseFloat(document.getElementById('quality').value);
+      let guests = parseFloat(document.getElementById('guests').value);
+      let extra = parseFloat((total*percentage).toFixed(2));
+      let full_bill = parseFloat((total + extra).toFixed(2));
+      let shares = (full_bill/guests).toFixed(2);        
+      this.tip = extra;
+      this.final = full_bill;
+      this.split = shares;
+        }
+  }
+  
+  function displayOut(){
+      let meal = new Dinner();
+        let htmlString1 = "Amount of tip: $"+ meal.tip;
+        let htmlString2 = "Each person pays: $"+ meal.split;
+        let htmlString3 = "Full bill is: $"+ meal.final;
+    document.getElementById("gratuity").innerHTML = htmlString1;
+    document.getElementById("eachguest").innerHTML = htmlString2;
+    document.getElementById("totalof").innerHTML = htmlString3;
+  }
